2c497f1015139d7835c27b58f834a4a113d38d07
Ville Tirroniemi <ville_tirroniemi@hotmail.com>
2021-12-14 15:09:07 +0200

    eslint

--- models/person.js
+++ models/person.js
@@ -7,25 +7,25 @@ console.log('connecting to', url)
 
 mongoose.connect(url)
   .then(result => {
-    console.log('Connected to MongoDB')    
-})
-.catch(error => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
     console.log('Error connecting to MongoDB', error.message)
-})
+  })
 
 const phonebookSchema = new mongoose.Schema({
-    name: {type: String, required: true, minlength: 3, unique: true},
-    number: {type: String, required: true, minlength: 8, unique: true},
-}, {"collection": "persons"})
+  name: { type: String, required: true, minlength: 3, unique: true },
+  number: { type: String, required: true, minlength: 8, unique: true },
+}, { 'collection': 'persons' })
 
 phonebookSchema.plugin(uniqueValidator)
 
 phonebookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', phonebookSchema)
